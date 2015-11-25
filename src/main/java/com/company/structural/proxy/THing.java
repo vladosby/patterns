26d6b0e38c8e720e565d33e4121105695705a0a5
Uladzislau Kiyeuski <Uladzislau_Kiyeuski@epam.com>
2015-11-25 10:25:54 +0300

    change structure of the project

--- src/main/java/com/company/structural/proxy/THing.java
+++ src/main/java/com/company/structural/proxy/THing.java
@@ -7,6 +7,6 @@ import java.util.Date;
  */
 public abstract class Thing {
     public void sayHello() {
-        System.out.println(this.getClass().getSimpleName() + "Create time = " + new Date());
+        System.out.println(this.getClass().getSimpleName() + " Create time = " + new Date());
     }
 }
